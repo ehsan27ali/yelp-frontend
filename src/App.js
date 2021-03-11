147746eec81195d45ebdf5e35d7e61ed99a14baa
Ehsan Ali <ehsan27ali@gmail.com>
2021-03-11 02:01:23 -0600

    adding react-router-dom, set up routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import { LandingPage } from "./LandingPage/LandingPage";
+import React from 'react';
+import {LandingPage} from './LandingPage/LandingPage';
+import {Route, Switch} from 'react-router-dom';
+import {Search} from './Search/Search';
 
 function App() {
   return (
-    <LandingPage/>
+    <Switch>
+      <Route path='/search' component={Search}></Route>
+      <Route path='/' component={LandingPage}></Route>
+    </Switch>
 
   );
 }
